c9525dd136d245988071da1bf2924e817a2edf96
unknown <chauhanarya79@gmail.com>
2021-09-17 21:16:29 +0530

    first commit

--- script.js
+++ script.js
@@ -27,7 +27,47 @@ tl.from('#one', {
     duration : 11
 }, '-=11')
 
+tl.from('#oneint', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
 
+tl.from('#two', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
+
+tl.from('#three', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
+
+tl.from('#four', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
+
+tl.from('#five', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
+
+tl.from('#six', {
+    height: 0,
+    opacity : 0,
+    ease : Expo.easeInOut,
+    duration : 11
+}, '-=11')
 // .from('#one', {
 //     height: 0,
 //     opacity : 0,
